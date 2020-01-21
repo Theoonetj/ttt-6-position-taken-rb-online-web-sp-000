462f625dab1358cfe6a72004589b6800fe70635e
confident-token-1019 <theoonetj@gmail.com>
2020-01-21 02:35:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,24 @@
 # code your #position_taken? method here!
 
-def position_taken?(the_board, the_index)
+ttt_board = ["",""," "," ","X"," "," "," ",""]
+
+def position_taken?(board, index)
+  # basic solution:
+
+   taken = nil
+   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+     taken = false
+   else
+    taken = true
+   end
+  # taken
+
+  # advanced solution w/ ternary operator
   
+  # This was flatiron School's solution (Learn.co) whereas mine was simpler
+  #! (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  #  false : true
+
+  #This was mine:
+  #! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
 end
